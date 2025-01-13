e16375771a5438aefb8c3c34e25132263fdb4e49
valkyraycho <razor0424@gmail.com>
2025-01-13 06:11:03 -0800

    update catalog server

--- catalog/server.go
+++ catalog/server.go
@@ -1,1 +1,83 @@
 package catalog
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	pb "github.com/valkyraycho/go-microservices/catalog/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type catalogServer struct {
+	pb.CatalogServiceServer
+	service Service
+}
+
+func ListenGRPC(s Service, port int) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+
+	if err != nil {
+		return err
+	}
+
+	server := grpc.NewServer()
+	pb.RegisterCatalogServiceServer(server, &catalogServer{service: s})
+	reflection.Register(server)
+	return server.Serve(lis)
+}
+
+func (s *catalogServer) PostProduct(ctx context.Context, r *pb.PostProductRequest) (*pb.PostProductResponse, error) {
+	p, err := s.service.PostProduct(ctx, r.Name, r.Description, r.Price)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.PostProductResponse{Product: &pb.Product{
+		Id:          p.ID,
+		Name:        p.Name,
+		Description: p.Description,
+		Price:       p.Price,
+	}}, nil
+}
+func (s *catalogServer) GetProduct(ctx context.Context, r *pb.GetProductRequest) (*pb.GetProductResponse, error) {
+	p, err := s.service.GetProduct(ctx, r.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.GetProductResponse{Product: &pb.Product{
+		Id:          p.ID,
+		Name:        p.Name,
+		Description: p.Description,
+		Price:       p.Price,
+	}}, nil
+
+}
+func (s *catalogServer) GetProducts(ctx context.Context, r *pb.GetProductsRequest) (*pb.GetProductsResponse, error) {
+	var res []Product
+	var err error
+
+	if len(r.Ids) != 0 {
+		res, err = s.service.GetProductsByIDs(ctx, r.Ids)
+	} else if r.Query != "" {
+		res, err = s.service.SearchProducts(ctx, r.Query, r.Skip, r.Take)
+	} else {
+		res, err = s.service.GetProducts(ctx, r.Skip, r.Take)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	products := []*pb.Product{}
+
+	for _, p := range res {
+		products = append(products, &pb.Product{
+			Id:          p.ID,
+			Name:        p.Name,
+			Description: p.Description,
+			Price:       p.Price,
+		})
+	}
+	return &pb.GetProductsResponse{Products: products}, nil
+}
