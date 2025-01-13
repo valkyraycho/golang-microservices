04dfb5501873c03282db9deeeb173aa975bc7322
valkyraycho <razor0424@gmail.com>
2025-01-13 08:19:29 -0800

    update catalog main

--- catalog/cmd/catalog/main.go
+++ catalog/cmd/catalog/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"log"
+	"time"
 
+	"github.com/kelseyhightower/envconfig"
+	"github.com/tinrab/retry"
+	"github.com/valkyraycho/go-microservices/catalog"
+)
+
+type Config struct {
+	DatabaseURL string `envconfig:"DATABASE_URL"`
+}
+
+func main() {
+	var cfg Config
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var r catalog.Repository
+
+	retry.ForeverSleep(2*time.Second, func(i int) error {
+		r, err = catalog.NewElasticRepository(cfg.DatabaseURL)
+		if err != nil {
+			log.Println(err)
+		}
+		return nil
+	})
+	defer r.Close()
+
+	log.Println("Listening on port 8080...")
+	s := catalog.NewService(r)
+	log.Fatal(catalog.ListenGRPC(s, 8080))
 }
